694f780ab8b22b2784dfafc1d45ea4e00d6cedb2
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-25 19:15:41 +0100

    all post verification oo documents done

--- server.js
+++ server.js
@@ -27,14 +27,28 @@ app.post('/api/books', function (request, response) {
     const db = client.db('literature')
     const collection = db.collection('books')
     const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      author_birth_year: request.body.author_birth_year,
-      author_death_year: Number(request.body.author_death_year),
-      url: request.body.url,
+      title: request.body.title ? request.body.title : null,
+      author: request.body.author ? request.body.author : null,
+      author_birth_year: request.body.author_birth_year
+        ? request.body.author_birth_year
+        : null,
+      author_death_year: request.body.author_death_year
+        ? Number(request.body.author_death_year)
+        : null,
+      url: request.body.url ? request.body.url : null,
+    }
+    if (
+      newBook.title === null ||
+      newBook.author === null ||
+      newBook.author_birth_year === null ||
+      newBook.author_death_year === null ||
+      newBook.url === null
+    ) {
+      client.close()
+      return response.sendStatus(400)
     }
     collection.insertOne(newBook, (err, result) => {
-      response.send(err || result)
+      response.send(err || result.ops[0])
       client.close()
     })
   })
