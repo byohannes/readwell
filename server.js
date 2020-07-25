2b859daf331bef52d403684770951471eeaa66c9
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-25 18:45:12 +0100

    post new books done

--- server.js
+++ server.js
@@ -6,11 +6,34 @@ const uri = process.env.URI
 const app = express()
 app.use(express.json())
 app.get('/', function (request, response) {
-  const client = new mongodb.MongoClient(uri)
+  const client = new mongodb.MongoClient(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
   client.connect(function () {
     const db = client.db('literature')
     const collection = db.collection('books')
     collection.find().toArray((err, result) => {
+      response.send(err || result)
+      client.close()
+    })
+  })
+})
+
+app.post('/api/books', function (request, response) {
+  const client = new mongodb.MongoClient(uri)
+  client.connect(function () {
+    const db = client.db('literature')
+    const collection = db.collection('books')
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      author_birth_year: request.body.author_birth_year,
+      author_death_year: Number(request.body.author_death_year),
+      url: request.body.url,
+    }
+    collection.insertOne(newBook, (err, result) => {
       response.send(err || result)
       client.close()
     })
